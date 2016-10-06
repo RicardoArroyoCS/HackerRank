de48a8a7f272efcef7626ccef00f03a4d8dd901d
Ricardo Arroyo <ricardo0arroyo2@gmail.com>
2016-10-06 18:21:41 -0500

    Initial

---
+++ HackerRank/Challenge/CommonSubstring.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HackerRank
+{
+    class CommonSubstring
+    {
+        public static void commonSubstring(string[] a, string[] b)
+        {
+            if(a.Length == b.Length)
+            {
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if(a[i].Contains(b[i]))
+                    {
+                        Console.WriteLine("YES");
+                    }
+                    else
+                    {
+                        Console.WriteLine("NO");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ HackerRank/Challenge/MinimumAmount.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HackerRank
+{
+    public class MinimumAmount
+    {
+    }
+}
